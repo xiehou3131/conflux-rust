d9566d96c58279e34558b7819ad5a0865c8d2915
xiehou3131 <xiehou@sjtu.edu.cn>
2025-02-07 11:42:15 +0800

    chore

--- crates/cfxcore/core/src/sync/message/transactions.rs
+++ crates/cfxcore/core/src/sync/message/transactions.rs
@@ -567,7 +567,7 @@ impl Handleable for GetTransactionsResponse {
     fn handle(self, ctx: &Context) -> Result<(), Error> {
         let _timer = MeterTimer::time_func(TX_HANDLE_TIMER.as_ref());
 
-        debug!("on_get_transactions_response {:?}", self.request_id);
+        info!("on_get_transactions_response {:?}", self.request_id);
 
         let req = ctx.match_request(self.request_id)?;
         let req = req.downcast_ref::<GetTransactions>(
@@ -577,7 +577,7 @@ impl Handleable for GetTransactionsResponse {
 
         // FIXME: Do some check based on transaction request.
 
-        debug!(
+        info!(
             "Received {:?} transactions and {:?} tx hashes from Peer {:?}",
             self.transactions.len(),
             self.tx_hashes.len(),
